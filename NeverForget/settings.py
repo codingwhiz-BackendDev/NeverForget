873d285ccf9508fe4ebe26cc0e794165a346d111
Emmanuel Oluyemi <e8060097@gmail.com>
2025-09-02 23:51:19 +0100

    Cloidinary fixed

--- NeverForget/settings.py
+++ NeverForget/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY = 'django-insecure-=f=e#jlj23p!od!h@5rqnc*=hxw#g**z4=&fl+hmq!v!9^j6at
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Read DEBUG from environment; default to False in production
-DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
-ALLOWED_HOSTS = ['neverforget-yjhp.onrender.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
