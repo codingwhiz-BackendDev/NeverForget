f0e3f0f3b019c97909ae6e6e83ed4e800276def0
Emmanuel Oluyemi <e8060097@gmail.com>
2025-09-02 17:41:25 +0100

    Still battling with cloudinary maybe last fix , omo i don tire ooo

--- NeverForget/settings.py
+++ NeverForget/settings.py
@@ -38,14 +38,13 @@ INSTALLED_APPS = [
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUD_NAME'),
     'API_KEY': os.getenv('API_KEY'),
-    'API_SECRET': os.getenv('API_SECRET'), 
+    'API_SECRET': os.getenv('API_SECRET'),
+    'STATIC_TAG': 'neverforget',
 }
 
 # Set Cloudinary as the default storage for media files
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# Media files configuration
-MEDIA_URL = '/media/'
+ 
  
 
 MIDDLEWARE = [
